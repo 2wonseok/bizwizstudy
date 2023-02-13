4e31c8ee5f725b852383fbf5533e6ed7ef496870
LeeHyeonHo <leehh@bizwizsystem.co.kr>
2023-02-13 18:32:38 +0900

    programmers 폰켓몬 추가

---
+++ src/bizwiz/programmers/level1/Phonekemon.java
@@ -0,0 +1,17 @@
+package bizwiz.programmers.level1;
+
+import java.util.Arrays;
+
+public class Phonekemon {
+    public static void main(String[] args)  {
+        int[] Phonekemons = {1,5,2,6,3,4};
+        System.out.println(solution(Phonekemons));
+    }
+
+    public static int solution(int[] nums) {
+        int answer = nums.length / 2;
+        int di = (int) Arrays.stream(nums).distinct().count();
+        return answer > di ? di : answer;
+    }
+
+}
