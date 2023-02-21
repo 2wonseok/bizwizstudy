4971f456065c11e4157ac8182ae1c89570ee98aa
cspark <cspark30@bizwizsystem.co.kr>
2023-02-21 10:59:36 +0900

    프로그래머스 2회차

---
+++ src/main/java/programers/second/문자열_내_마음대로_정렬하기.java
@@ -0,0 +1,82 @@
+package programers.second;
+
+import java.util.*;
+
+
+/* 문제 설명
+문자열로 구성된 리스트 strings와, 정수 n이 주어졌을 때, 각 문자열의 인덱스 n번째 글자를 기준으로 오름차순 정렬하려 합니다.
+예를 들어 strings가 ["sun", "bed", "car"]이고 n이 1이면 각 단어의 인덱스 1의 문자 "u", "e", "a"로 strings를 정렬합니다.
+제한 조건
+	• strings는 길이 1 이상, 50이하인 배열입니다.
+	• strings의 원소는 소문자 알파벳으로 이루어져 있습니다.
+	• strings의 원소는 길이 1 이상, 100이하인 문자열입니다.
+	• 모든 strings의 원소의 길이는 n보다 큽니다.
+	• 인덱스 1의 문자가 같은 문자열이 여럿 일 경우, 사전순으로 앞선 문자열이 앞쪽에 위치합니다.
+입출력 예
+strings	n	return
+["sun", "bed", "car"]	1	["car", "bed", "sun"]
+["abce", "abcd", "cdx"]	2	["abcd", "abce", "cdx"]
+입출력 예 설명
+입출력 예 1
+"sun", "bed", "car"의 1번째 인덱스 값은 각각 "u", "e", "a" 입니다. 이를 기준으로 strings를 정렬하면 ["car", "bed", "sun"] 입니다.
+입출력 예 2
+"abce"와 "abcd", "cdx"의 2번째 인덱스 값은 "c", "c", "x"입니다. 따라서 정렬 후에는 "cdx"가 가장 뒤에 위치합니다. "abce"와 "abcd"는
+사전순으로 정렬하면 "abcd"가 우선하므로, 답은 ["abcd", "abce", "cdx"] 입니다. */
+
+
+public class 문자열_내_마음대로_정렬하기 {
+    
+        public String[] solution(String[] strings, int n) {
+            String[] answer = new String[strings.length];
+         
+         //unique indexList 만들고 정렬        
+            Set<String> set = new HashSet<>();        
+            for(String str : strings){
+                set.add(str.substring(n,n+1));
+            }
+            List<String> indexStrList = new ArrayList<>(set);
+            Collections.sort(indexStrList);
+    
+         //index를 key로 filter로 그룹을 만들고 그것들을 다시 정렬해서 list에 순서대로 넣음
+            List<String> resultList = new ArrayList<>();
+          
+            for(String indexStr : indexStrList){
+                String[] valArr = Arrays.stream(strings).filter(x -> x.substring(n,n+1).equals(indexStr)).toArray(String[]::new);
+                List<String> valList = Arrays.asList(valArr);
+                Collections.sort(valList);
+                for(String val : valList){
+                    resultList.add(val);
+                }
+            }
+            
+         //String[]로 변환
+            for(int i = 0; i < resultList.size(); i++){
+                answer[i] = resultList.get(i);
+            }
+            
+            return answer;
+        }
+
+
+
+
+
+
+        /* 다른사람풀이 */
+        class Solution {
+            public String[] solution(String[] strings, int n) {
+                String[] answer = {};
+                ArrayList<String> arr = new ArrayList<>();
+                for (int i = 0; i < strings.length; i++) {
+                    arr.add("" + strings[i].charAt(n) + strings[i]);
+                }
+                Collections.sort(arr);
+                answer = new String[arr.size()];
+                for (int i = 0; i < arr.size(); i++) {
+                    answer[i] = arr.get(i).substring(1, arr.get(i).length());
+                }
+                return answer;
+            }
+        }
+    
+}
