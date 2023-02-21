2d9095aeb52b1447a25fe4efc39a3a5475fbd0ee
LeeHyeonHo <leehh@bizwizsystem.co.kr>
2023-02-21 11:09:04 +0900

    문자열 내 p와 y의 개수 추가

---
+++ src/bizwiz/programmers/level1/문자열내p와y의개수.java
@@ -0,0 +1,28 @@
+package bizwiz.programmers.level1;
+
+public class 문자열내p와y의개수 {
+
+    public static void main(String[] args) {
+
+
+        System.out.println(solution("pPoooyYkdlsadsmdkfnmdkfdkfeippY"));
+
+
+    }
+
+    public static boolean solution(String s) {
+        int pCount = 0;
+        int yCount = 0;
+
+        for (char c : s.toCharArray()) {
+            if(Character.toString(c).equalsIgnoreCase("p")) {
+                pCount += 1;
+            } else if (Character.toString(c).equalsIgnoreCase("y")) {
+                yCount += 1;
+            }
+        }
+
+        return pCount == yCount;
+    }
+
+}
