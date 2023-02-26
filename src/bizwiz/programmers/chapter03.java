39247cc8a41edb6ea159b105ac4e11de31639f97
김재범 <jabum2000@bizwizsystem.co.kr>
2023-02-26 17:55:18 +0900

    수박

---
+++ src/bizwiz/programmers/chapter03.java
@@ -0,0 +1,27 @@
+package bizwiz.programmers;
+
+public class chapter03 {
+    public static void main(String[] args) {
+        new chapter03().solution(4);
+    }
+
+/*
+수박수박수박수박수
+문제 설명
+길이가 n이고, "수박수박수박수...."와 같은 패턴을 유지하는 문자열을 리턴하는 함수,solution을 완성하세요.
+예를들어 n이 4이면 "수박수박"을 리턴하고 3이라면 "수박수"를 리턴하면 됩니다.
+ */
+public String solution(int n) {
+    String answer="";
+    for (int i=0;i<n;i++){
+    if(i%2==0){
+      answer+="수";
+    }else{
+        answer+="박";
+    }
+    }
+    System.out.println(answer);
+    return answer;
+}
+
+}
