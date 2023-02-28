72f45a0803e470337bf85181853623341aaf9ebe
LeeHyeonHo <leehh@bizwizsystem.co.kr>
2023-02-28 10:51:28 +0900

    s

---
+++ src/bizwiz/programmers/level1/c약수의합.java
@@ -0,0 +1,22 @@
+package bizwiz.programmers.level1;
+
+public class c약수의합 {
+
+    public static void main(String[] args) {
+        solution(12);
+    }
+
+    public static int solution(int n) {
+        int answer = 0;
+
+
+        for (int i = 1; i <= n; i++) {
+            if(n % i == 0) {
+                answer += i;
+            }
+        }
+
+        return answer;
+    }
+
+}
